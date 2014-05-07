46c92afc69ea6b054e6adab8150b7398d4c9f34a
bmrxntfj <bmrxntfj@163.com>
2014-05-07 12:56:28 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+﻿var routes = [
+{ pattern: /^\/(default.htm|favicon.ico|(resources\/.*))$/, handle: require('./route/staticResource.js') },
+{ pattern: /^\/\w+\/\w+((\?.*)?)$/, handle: require('./route/dataservice.js')}];
+console.log('s');
+var httpserver = require('httpserver');
+httpserver.run(routes);
