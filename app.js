69f9e7163e38e982cc2997b31b02c48b590d8980
bmrxntfj <bmrxntfj@163.com>
2014-05-12 16:09:46 +0800

    fix log path bug

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-﻿var app={
+﻿var path=require('path');
+var app={
 	host:'127.0.0.1',
 	port:'1337',
+	rootPath:path.normalize('.'),
 	uploads:[{pattern:/^\/upload$/,path:'./web/upload/'}],
 	routes: [
 	{ pattern: /^\/(favicon.ico|(web\/.*))$/, handle: require('staticResource.js') },
